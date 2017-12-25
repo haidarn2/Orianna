0b52d409fec3045ca06b7ecba11edd6445c9f536
unknown <nadeem.haidar@mail.utoronto.ca>
2017-12-24 21:36:23 -0500

    Initial commit

---
+++ src/test/java/com/orianna/orianna/OriannaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.orianna.orianna;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OriannaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
